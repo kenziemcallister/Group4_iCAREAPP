b7822dfb537d77fbd4a36db781fb70ba3e92b909
kenziemcallister <mackenziemcallister64@gmail.com>
2024-10-27 19:13:41 -0500

    Created ImportImage and the form for uploading an image. When we upload an image (file) we have to attach the document meta data and modification history for the file that was uploaded. It is not finished yet but the button should be on the home page

--- Group4_iCAREAPP/Controllers/ImportImageController.cs
+++ Group4_iCAREAPP/Controllers/ImportImageController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Group4_iCAREAPP.Models;
+using System.Diagnostics;
 
 namespace Group4_iCAREAPP.Controllers
 {
@@ -39,29 +41,57 @@ namespace Group4_iCAREAPP.Controllers
         // GET: ImportImage/Create
         public ActionResult Create()
         {
-            ViewBag.userID = new SelectList(db.iCareWorker, "ID", "profession");
-            ViewBag.docID = new SelectList(db.ModificationHistory, "ID", "description");
-            return View();
+            // Generate a unique 10-character ID
+            string docID = Guid.NewGuid().ToString("N").Substring(0, 10);
+
+            // Initialize DocumentMetadata with pre-set docID
+            var documentMetadata = new DocumentMetadata
+            {
+                docID = docID
+            };
+
+            var users = db.iCareUser.ToList();
+            ViewBag.userID = new SelectList(users, "ID", "ID");
+            return View(documentMetadata); // Pass initialized object to the view
         }
 
         // POST: ImportImage/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "docID,userID,docName,dateOfCreation,versions")] DocumentMetadata documentMetadata)
+        public ActionResult Create(HttpPostedFileBase file, [Bind(Include = "docID,userID,docName,dateOfCreation,versions")] DocumentMetadata documentMetadata, [Bind(Include = "dateOfModification,description")] ModificationHistory modificationHistory)
         {
-            if (ModelState.IsValid)
+            if (file != null && file.ContentLength > 0 && ModelState.IsValid)
             {
+                // Save the file to a directory
+                string filePath = Path.Combine(Server.MapPath("~/UploadedImages"), Path.GetFileName(file.FileName));
+                file.SaveAs(filePath);
+
+                // Set Document Metadata properties
+                documentMetadata.docID = Guid.NewGuid().ToString().Substring(0, 10); // Ensure it is 10 characters
+                documentMetadata.docName = Path.GetFileName(file.FileName);
+                documentMetadata.dateOfCreation = DateTime.Now;
+
+                // Set Modification History properties
+                modificationHistory.ID = documentMetadata.docID; // Match IDs
+                modificationHistory.dateOfModification = DateTime.Now; // or use the posted value if it should come from the form
+
+                // Add to the database
+                db.ModificationHistory.Add(modificationHistory);
+
+                // Populate versions from the submitted modification history date
+                documentMetadata.versions = modificationHistory.dateOfModification?.ToString("yyyy-MM-dd"); // Set the versions field as a formatted string
+
                 db.DocumentMetadata.Add(documentMetadata);
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
 
             ViewBag.userID = new SelectList(db.iCareWorker, "ID", "profession", documentMetadata.userID);
-            ViewBag.docID = new SelectList(db.ModificationHistory, "ID", "description", documentMetadata.docID);
             return View(documentMetadata);
         }
+
+
 
         // GET: ImportImage/Edit/5
         public ActionResult Edit(string id)

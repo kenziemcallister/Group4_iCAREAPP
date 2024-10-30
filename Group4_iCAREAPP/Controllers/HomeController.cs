f6eefb3789d5e2b25e88ca23d6743075b5cc2e17
kenziemcallister <mackenziemcallister64@gmail.com>
2024-10-30 00:31:27 -0500

    Fixed database stuff, and made it so that when you login depending on your role you see different things

--- Group4_iCAREAPP/Controllers/HomeController.cs
+++ Group4_iCAREAPP/Controllers/HomeController.cs
@@ -1,32 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using Group4_iCAREAPP.Models; // Ensure this namespace is included for your models
 
 namespace Group4_iCAREAPP.Controllers
 {
     public class HomeController : Controller
     {
+        private Group4_iCAREDBEntities db = new Group4_iCAREDBEntities();
+
         public ActionResult Index()
         {
+            // Fetch the logged-in user's data
+            var userId = User.Identity.Name; // Adjust this based on how you get the user ID
+            var currentUser = db.iCareUser.FirstOrDefault(u => u.ID == userId);
+            var currentWorker = db.iCareWorker.FirstOrDefault(w => w.ID == userId);
+
+            // Store the user and worker information in the ViewBag for use in the layout
+            ViewBag.CurrentUser = currentUser;
+            ViewBag.CurrentWorker = currentWorker;
+
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
-
-        
     }
 }
